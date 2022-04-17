7666dce183d0c92abe3af32dffc5e584bc14c164
Jonathan Strickland <djanatyn@gmail.com>
2022-04-17 18:34:10 -0400

    set correct length

--- listdevs.c
+++ listdevs.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include "libusb.h"
 
+const int LENGTH = 37;
+const int MAYFLASH_ENDPOINT = 0x81;
+const int MAYFLASH_VID = 0x057e;
+const int MAYFLASH_PID = 0x0337;
 
 int main(void)
 {
 	// init libusb
 	if (libusb_init(NULL) < 0) { return 1; }
 
-	// adapter vid is 0x057e
-	// adapter pid is 0x0337
-	libusb_device_handle *handle = libusb_open_device_with_vid_pid(NULL, 0x057e, 0x0337);
+	// get mayflash handle
+	libusb_device_handle *handle =
+		libusb_open_device_with_vid_pid(NULL, MAYFLASH_VID, MAYFLASH_PID);
 
-	// check for handle
+	// abort if handle not found
 	if (handle != NULL) {
 		printf("device found: 0x%X\n", handle);
 	} else {
 		printf("device not found\n");
+		return 1;
 	}
 
-	const int LENGTH = 1;
+	// prepare buffer for usb interrupt transfer
 	unsigned char data[LENGTH];
 	int ret, len;
-	ret = libusb_interrupt_transfer(handle, 0x81, data, LENGTH, &len, 0);
+	ret = libusb_interrupt_transfer(handle, MAYFLASH_ENDPOINT, data, LENGTH, &len, 0);
 
 	if (ret != 0) {
 		printf("failed\n");
@@ -29,11 +34,9 @@ int main(void)
 		printf("len: %d\n", len);
 		printf("data: %d\n", data);
 	} else {
-		printf("%d\n", len);
 		printf("%x\n", data);
 	}
 
-	// quit
 	libusb_exit(NULL);
 	return 0;
 }
