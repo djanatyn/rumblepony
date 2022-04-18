9f895b9ecbfa1f62bf9eac5f3a8ad5755814f52e
Jonathan Strickland <djanatyn@gmail.com>
2022-04-18 19:56:18 -0400

    check_controller

--- read-gcc.c
+++ read-gcc.c
@@ -30,17 +30,61 @@ struct AdapterHandle {
 const int ERR_ADAPTER_NOT_FOUND = 1;
 
 // try to open mayflash adapter, setting handle
-int open_adapter(struct AdapterHandle *adapter_handle) {
+int open_adapter(struct AdapterHandle *adapter) {
 	libusb_device_handle *handle =
 		libusb_open_device_with_vid_pid(NULL, MAYFLASH_VID, MAYFLASH_PID);
 
 	// abort if handle not found
 	if (handle != NULL) {
-		adapter_handle->handle = handle;
+		adapter->handle = handle;
 		return 0;
 	} else {
 		return ERR_ADAPTER_NOT_FOUND;
 	}
+}
+
+const int ERR_UNEXPECTED_LENGTH = 1;
+const int ERR_NO_MAGIC = 2;
+
+int check_controller(struct AdapterHandle *adapter, struct ControllerInfo *info) {
+	unsigned char data[LENGTH];
+	int r, len;
+	r = libusb_interrupt_transfer(adapter->handle, MAYFLASH_ENDPOINT, data, LENGTH, &len, 0);
+
+	if (r != 0) {
+		fprintf(stderr, "[check_controller] failed\n");
+		fprintf(stderr, "[check_controller] r: %d\n", r);
+		fprintf(stderr, "[check_controller] len: %d\n", len);
+		return ERR_UNEXPECTED_LENGTH;
+	} else {
+		/* for (int i = 0; i < LENGTH; i++) { */
+		/* 	putchar(data[i]); */
+		/* } */
+
+		if (*data != MAGIC)  {
+			fprintf(stderr, "[check_controller] did not find magic number: %#x", *data);
+			return ERR_NO_MAGIC;
+		} else {
+			unsigned char *startp1 = data + 1; // skip magic
+			unsigned char *startp2 = startp1 + CONTROLLER_WIDTH;
+			unsigned char *startp3 = startp2 + CONTROLLER_WIDTH;
+			unsigned char *startp4 = startp3 + CONTROLLER_WIDTH;
+
+			struct Controller p1, p2, p3, p4;
+			memcpy(&(info->p1.a), startp1 + A_BUTTON_OFFSET, sizeof(bool));
+			memcpy(&(info->p2.a), startp2 + A_BUTTON_OFFSET, sizeof(bool));
+			memcpy(&(info->p3.a), startp3 + A_BUTTON_OFFSET, sizeof(bool));
+			memcpy(&(info->p4.a), startp4 + A_BUTTON_OFFSET, sizeof(bool));
+
+			printf(info->p1.a ? "p1: true\n" : "p1: false\n");
+			printf(info->p2.a ? "p2: true\n" : "p2: false\n");
+			printf(info->p3.a ? "p3: true\n" : "p3: false\n");
+			printf(info->p4.a ? "p4: true\n" : "p4: false\n");
+
+			return 0;
+		}
+	}
+
 }
 
 int main(void)
@@ -53,7 +97,7 @@ int main(void)
 	r = open_adapter(&adapter);
 	switch (r) {
 		case 0:
-			fprintf(stderr, "device found: 0x%X\n", adapter.handle);
+			fprintf(stderr, "device found: %p\n", adapter.handle);
 			break;
 		case ERR_ADAPTER_NOT_FOUND:
 			fprintf(stderr, "device not found\n");
@@ -63,42 +107,8 @@ int main(void)
 			return 1;
 	};
 
-	// prepare buffer for usb interrupt transfer
-	unsigned char data[LENGTH];
-	int ret, len;
-	ret = libusb_interrupt_transfer(adapter.handle, MAYFLASH_ENDPOINT, data, LENGTH, &len, 0);
-
-	if (ret != 0) {
-		fprintf(stderr, "failed\n");
-		fprintf(stderr, "ret: %d\n", ret);
-		fprintf(stderr, "len: %d\n", len);
-		return 1;
-	} else {
-		/* for (int i = 0; i < LENGTH; i++) { */
-		/* 	putchar(data[i]); */
-		/* } */
-
-		if (*data != MAGIC)  {
-			fprintf(stderr, "did not find magic number: %#x", *data);
-			return 1;
-		} else {
-			unsigned char *startp1 = data + 1; // skip magic
-			unsigned char *startp2 = startp1 + CONTROLLER_WIDTH;
-			unsigned char *startp3 = startp2 + CONTROLLER_WIDTH;
-			unsigned char *startp4 = startp3 + CONTROLLER_WIDTH;
-
-			struct Controller p1, p2, p3, p4;
-			memcpy(&p1.a, startp1 + A_BUTTON_OFFSET, sizeof(bool));
-			memcpy(&p2.a, startp2 + A_BUTTON_OFFSET, sizeof(bool));
-			memcpy(&p3.a, startp3 + A_BUTTON_OFFSET, sizeof(bool));
-			memcpy(&p4.a, startp4 + A_BUTTON_OFFSET, sizeof(bool));
-
-			printf(p1.a ? "p1: true\n" : "p1: false\n");
-			printf(p2.a ? "p2: true\n" : "p2: false\n");
-			printf(p3.a ? "p3: true\n" : "p3: false\n");
-			printf(p4.a ? "p4: true\n" : "p4: false\n");
-		}
-	}
+	struct ControllerInfo info;
+	r = check_controller(&adapter, &info);
 
 	libusb_exit(NULL);
 	return 0;
